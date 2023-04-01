bf2e49ab4d42482b9f931660dfca32cd291142b8
Rokas1212 <rokas.roktantu@gmail.com>
2023-04-02 02:00:52 +0300

    Fix bugs with displaying images
    Fixed image not displaying after predicting

--- src/static/js/image_upload.js
+++ src/static/js/image_upload.js
@@ -3,14 +3,28 @@
 * ========================================== */
 function readURL(input) {
     if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $('#imageResult')
-                .attr('src', e.target.result);
-        };
-        reader.readAsDataURL(input.files[0]);
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#imageResult').attr('src', e.target.result);
+        localStorage.setItem('imageData', e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
     }
-}
+  }
+
+
+$('#buttonas').click(function() {
+    var imageData = localStorage.getItem('imageData');
+    if (imageData) {
+      sessionStorage.setItem('imageData', imageData);
+    }
+    window.location.href = 'next-page.html';
+  });
+  
+  var imageData = sessionStorage.getItem('imageData');
+  if (imageData) {
+    $('#imageResult').attr('src', imageData);
+  }
 
 $(function () {
     $('#upload').on('change', function () {
