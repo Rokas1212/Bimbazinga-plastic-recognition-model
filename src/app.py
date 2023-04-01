fb50b6bd98e427320a8e4a4812bfb1d52a3fa22c
konteris <matas.stankaitis@gmail.com>
2023-04-01 12:46:43 +0300

    added http requests

--- src/app.py
+++ src/app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from views import views 
+import pickle
 
-app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/views")
+# with open(f'model/.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/prediction', methods = ['POST'])
+def predict_plastic():
+    if 'image' not in request.files:
+        return 'No image found', 400
+    file = request.files['image']
+
+    if file.filename == '':
+        return 'No iamge found', 400
+    
+    if request.method == 'POST':
+        file = request.files['image']
+    file.save('/path/to/save/image')
+    return 'File uploaded successfully', 200
 
 if __name__ == '__main__':
     app.run(debug = True, port=8000)
